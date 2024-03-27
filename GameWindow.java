3c5e8ac9fcef293bf7c3832f80b611fdfe78146e
Kushraj Pollard <kushrajpollard@gmail.com>
2024-03-26 21:31:30 -0400

    Added GameWindow and GameApplication

---
+++ GameWindow.java
@@ -0,0 +1,211 @@
+import javax.swing.*;			// need this for GUI objects
+import java.awt.*;			// need this for Layout Managers
+import java.awt.event.*;		// need this to respond to GUI events
+	
+public class GameWindow extends JFrame 
+				implements ActionListener,
+					   KeyListener,
+					   MouseListener
+{
+	// declare instance variables for user interface objects
+
+	// declare labels 
+
+	private JLabel statusBarL;
+	private JLabel keyL;
+	private JLabel mouseL;
+
+	// declare text fields
+
+	private JTextField statusBarTF;
+	private JTextField keyTF;
+	private JTextField mouseTF;
+
+	// declare buttons
+
+	private JButton startB;
+	private JButton pauseB;
+	private JButton focusB;
+	private JButton exitB;
+
+	private Container c;
+
+	private JPanel mainPanel;
+
+	@SuppressWarnings({"unchecked"})
+	public GameWindow() {
+ 
+		setTitle ("A Game that Doesn't Really Do Anything");
+		setSize (500, 450);
+
+		// create user interface objects
+
+		// create labels
+
+		statusBarL = new JLabel ("Application Status: ");
+		keyL = new JLabel("Key Pressed: ");
+		mouseL = new JLabel("Location of Mouse Click: ");
+
+		// create text fields and set their colour, etc.
+
+		statusBarTF = new JTextField (25);
+		keyTF = new JTextField (25);
+		mouseTF = new JTextField (25);
+
+		statusBarTF.setEditable(false);
+		keyTF.setEditable(false);
+		mouseTF.setEditable(false);
+
+		statusBarTF.setBackground(Color.CYAN);
+		keyTF.setBackground(Color.YELLOW);
+		mouseTF.setBackground(Color.GREEN);
+
+		// create buttons
+
+		startB = new JButton ("Start");
+	    pauseB = new JButton ("Pause");
+	    focusB = new JButton ("Focus on Key");
+		exitB = new JButton ("Exit");
+
+		// add listener to each button (same as the current object)
+
+		startB.addActionListener(this);
+		pauseB.addActionListener(this);
+		focusB.addActionListener(this);
+		exitB.addActionListener(this);
+
+		
+		// create mainPanel
+
+		mainPanel = new JPanel();
+		FlowLayout flowLayout = new FlowLayout();
+		mainPanel.setLayout(flowLayout);
+
+		GridLayout gridLayout;
+
+		// create infoPanel
+
+		JPanel infoPanel = new JPanel();
+		gridLayout = new GridLayout(3, 2);
+		infoPanel.setLayout(gridLayout);
+		infoPanel.setBackground(Color.ORANGE);
+
+		// add user interface objects to infoPanel
+	
+		infoPanel.add (statusBarL);
+		infoPanel.add (statusBarTF);
+
+		infoPanel.add (keyL);
+		infoPanel.add (keyTF);		
+
+		infoPanel.add (mouseL);
+		infoPanel.add (mouseTF);
+
+
+		// create facePanel
+
+		JPanel facePanel = new FacePanel();
+		mainPanel.add (facePanel);
+		mainPanel.setVisible(true);
+		
+		// create buttonPanel
+
+		JPanel buttonPanel = new JPanel();
+		gridLayout = new GridLayout(1, 4);
+		buttonPanel.setLayout(gridLayout);
+
+		// add buttons to buttonPanel
+
+		buttonPanel.add (startB);
+		buttonPanel.add (pauseB);
+		buttonPanel.add (focusB);
+		buttonPanel.add (exitB);
+
+		// add sub-panels with GUI objects to mainPanel and set its colour
+
+		mainPanel.add(infoPanel);
+		mainPanel.add(buttonPanel);
+		mainPanel.setBackground(Color.PINK);
+
+		// set up mainPanel to respond to keyboard and mouse
+
+		mainPanel.addMouseListener(this);
+		mainPanel.addKeyListener(this);
+
+		// add mainPanel to window surface
+
+		c = getContentPane();
+		c.add(mainPanel);
+
+		// set properties of window
+
+		setResizable(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		setVisible(true);
+
+		// set status bar message
+
+		statusBarTF.setText("Application started.");
+	}
+
+
+	// implement single method in ActionListener interface
+
+	public void actionPerformed(ActionEvent e) {
+
+		String command = e.getActionCommand();
+		
+		statusBarTF.setText(command + " button clicked.");
+
+		if (command.equals(focusB.getText()))
+			mainPanel.requestFocus();
+	}
+
+
+	// implement methods in KeyListener interface
+
+	public void keyPressed(KeyEvent e) {
+		String keyText = e.getKeyText(e.getKeyCode());
+		keyTF.setText(keyText + " pressed.");
+	}
+
+	public void keyReleased(KeyEvent e) {
+
+	}
+
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	// implement methods in MouseListener interface
+
+	public void mouseClicked(MouseEvent e) {
+
+
+		int x = e.getX();
+		int y = e.getY();
+
+		mouseTF.setText("(" + x +", " + y + ")");
+
+	}
+
+
+	public void mouseEntered(MouseEvent e) {
+	
+	}
+
+	public void mouseExited(MouseEvent e) {
+	
+	}
+
+	public void mousePressed(MouseEvent e) {
+	
+	}
+
+	public void mouseReleased(MouseEvent e) {
+	
+	}
+
+}
+      
