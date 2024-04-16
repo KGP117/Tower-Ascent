1976caa71192cc4447a78d153a5fda7330a876a3
Sourav <91920705+SOURAV-jpeg@users.noreply.github.com>
2024-04-16 18:22:06 -0400

    Add files via upload

---
+++ TowerAscent/Sky.java
@@ -0,0 +1,34 @@
+import java.awt.Image;
+
+public class Sky extends Enemy {
+    private int health;
+    private int x;
+    private int y;
+    private Image image;
+
+    public Sky(int xPos, int yPos) {
+        super(xPos, yPos);
+        health=super.setHealth();        
+    }
+
+    public void attack() {
+        
+    }
+
+    public void draw() {
+
+    }
+
+    public void erase() {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void run() {
+    
+    }
+    
+}
---
+++ TowerAscent/Tile.java
@@ -0,0 +1,10 @@
+import java.awt.Image;
+
+public  class Tile {
+    Image tile;
+
+    public Tile(int i){
+        
+    }
+
+}
