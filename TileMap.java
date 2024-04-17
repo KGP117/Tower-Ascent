586599ceb9bb7def9025af71f2dec9c748be6bc7
Kushraj Pollard <kushrajpollard@gmail.com>
2024-04-17 03:13:29 -0400

    Adjusted tileMap size
    Adjusted the tileMap size from 32 to 64

--- TileMap.java
+++ TileMap.java
@@ -23,9 +23,9 @@ public class TileMap {
     private int offsetY;
 
     private LinkedList sprites;
-    //private Player player;
+    private Player player;
 
-    //BackgroundManager bgManager;
+    BackgroundManager bgManager;
 
     private JFrame window;
     private Dimension dimension;
@@ -45,30 +45,30 @@ public class TileMap {
 	mapWidth = width;
 	mapHeight = height;
 
-        // get the y offset to draw all sprites and tiles
+    // get the y offset to draw all sprites and tiles
 
-       	offsetY = screenHeight - tilesToPixels(mapHeight);
+    offsetY = screenHeight - tilesToPixels(mapHeight);
 	System.out.println("offsetY: " + offsetY);
 
-	//bgManager = new BackgroundManager (window, 12);
+	bgManager = new BackgroundManager (window, 12);
 
         tiles = new Image[mapWidth][mapHeight];
-	    //player = new Player (window, this, bgManager);
+	    player = new Player (window, this, bgManager);
         sprites = new LinkedList();
 
-	//Image playerImage = player.getImage();
-	//int playerHeight = playerImage.getHeight(null);
+	Image playerImage = player.getImage();
+	int playerHeight = playerImage.getHeight(null);
 
 	int x, y;
-	// x = (dimension.width / 2) + TILE_SIZE;	// position player in middle of screen
+	x = (dimension.width / 2) + TILE_SIZE;	// position player in middle of screen
 
 	x = 192;					// position player in 'random' location
-	//y = dimension.height - (TILE_SIZE + playerHeight);
+	y = dimension.height - (TILE_SIZE + playerHeight);
 
-        //player.setX(x);
-        //player.setY(y);
+        player.setX(x);
+        player.setY(y);
 
-	//System.out.println("Player coordinates: " + x + "," + y);
+	System.out.println("Player coordinates: " + x + "," + y);
 
     }
 
@@ -107,11 +107,13 @@ public class TileMap {
         bounds.
     */
     public Image getTile(int x, int y) {
-        if (x < 0 || x >= mapWidth ||
+        
+        if (x < 0 || x >= mapWidth || 
             y < 0 || y >= mapHeight)
         {
             return null;
         }
+
         else {
             return tiles[x][y];
         }
@@ -171,34 +173,29 @@ public class TileMap {
         // get the scrolling position of the map
         // based on player's position
 
-        //int offsetX = screenWidth / 2 - Math.round(player.getX()) - TILE_SIZE;
-        //offsetX = Math.min(offsetX, 0);
-        //offsetX = Math.max(offsetX, screenWidth - mapWidthPixels);
+        int offsetX = screenWidth / 2 - 
+            Math.round(player.getX()) - TILE_SIZE;
+        offsetX = Math.min(offsetX, 0);
+        offsetX = Math.max(offsetX, screenWidth - mapWidthPixels);
 
-/*
-        // draw black background, if needed
-        if (background == null ||
-            screenHeight > background.getHeight(null))
-        {
-            g.setColor(Color.black);
-            g.fillRect(0, 0, screenWidth, screenHeight);
-        }
-*/
 	// draw the background first
 
-	//bgManager.draw (g2);
+	bgManager.draw (g2);
 
         // draw the visible tiles
 
-        int firstTileX = pixelsToTiles(-1); // replace -1 with offset
+        int firstTileX = pixelsToTiles(-offsetX);
         int lastTileX = firstTileX + pixelsToTiles(screenWidth) + 1;
+        
         for (int y=0; y<mapHeight; y++) {
+            
             for (int x=firstTileX; x <= lastTileX; x++) {
                 Image image = getTile(x, y);
+                
                 if (image != null) {
-                    g2.drawImage(image,
-                        tilesToPixels(x) + 1, // replace 1 with offset
-                        tilesToPixels(y) + offsetY,
+                    g2.drawImage(image, 
+                        tilesToPixels(x) + offsetX, 
+                        tilesToPixels(y) + offsetY, 
                         null);
                 }
             }
@@ -207,10 +204,10 @@ public class TileMap {
 
         // draw player
 
-/*         g2.drawImage(player.getImage(),
+        g2.drawImage(player.getImage(),
             Math.round(player.getX()) + offsetX,
             Math.round(player.getY()), //+ offsetY,
-            null); */
+            null);
 
 /*
         // draw sprites
@@ -235,45 +232,45 @@ public class TileMap {
 
     public void moveLeft() {
 	int x, y;
-/* 	x = player.getX();
+	x = player.getX();
 	y = player.getY();
 
 	String mess = "Going left. x = " + x + " y = " + y;
 	System.out.println(mess);
 
-	player.move(1); */
+	player.move(1);
 
     }
 
 
     public void moveRight() {
 	int x, y;
-/* 	x = player.getX();
+	x = player.getX();
 	y = player.getY();
 
 	String mess = "Going right. x = " + x + " y = " + y;
 	System.out.println(mess);
 
-	player.move(2); */
+	player.move(2);
 
     }
 
 
     public void jump() {
 	int x, y;
-/* 	x = player.getX();
+	x = player.getX();
 	y = player.getY();
 
 	String mess = "Jumping. x = " + x + " y = " + y;
 	System.out.println(mess);
 
-	player.move(3); */
+	player.move(3);
 
     }
 
 
     public void update() {
-	//player.update();
+	    player.update();
     }
 
 }
