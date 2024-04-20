8dc7db278e16272b212e0e3f61b5ea959ca9b2cf
Kushraj Pollard <kushrajpollard@gmail.com>
2024-04-19 23:26:26 -0400

    Updated TileMapManager to Add Sprites

--- TileMapManager.java
+++ TileMapManager.java
@@ -16,24 +16,24 @@ public class TileMapManager {
     private ArrayList<Image> tiles;
     private JFrame window;
 
-/*
+
     private GraphicsConfiguration gc;
 
     // host sprites used for cloning
-    private Sprite playerSprite;
+    private Player playerSprite;
     private Sprite musicSprite;
     private Sprite coinSprite;
     private Sprite goalSprite;
     private Sprite grubSprite;
     private Sprite flySprite;
-*/
+
 
     public TileMapManager(JFrame window) {
 	this.window = window;
 
         loadTileImages();
 
-        //loadCreatureSprites();
+        loadCreatureSprites();
         //loadPowerUpSprites();
     }
 
@@ -107,7 +107,7 @@ public class TileMapManager {
     }
 
 
-/*
+
     private void addSprite(TileMap map,
         Sprite hostSprite, int tileX, int tileY)
     {
@@ -117,13 +117,13 @@ public class TileMapManager {
 
             // center the sprite
             sprite.setX(
-                TileMapRenderer.tilesToPixels(tileX) +
-                (TileMapRenderer.tilesToPixels(1) -
+                TileMap.tilesToPixels(tileX) +
+                (TileMap.tilesToPixels(1) -
                 sprite.getWidth()) / 2);
 
             // bottom-justify the sprite
             sprite.setY(
-                TileMapRenderer.tilesToPixels(tileY + 1) -
+                TileMap.tilesToPixels(tileY + 1) -
                 sprite.getHeight());
 
             // add it to the map
@@ -131,7 +131,7 @@ public class TileMapManager {
         }
     }
 
-*/
+
 
     // -----------------------------------------------------------
     // code for loading sprites and images
@@ -163,21 +163,21 @@ public class TileMapManager {
         }
     }
 
-/*
+
     public void loadCreatureSprites() {
 
         Image[][] images = new Image[4][];
 
         // load left-facing images
         images[0] = new Image[] {
-            loadImage("player1.png"),
-            loadImage("player2.png"),
-            loadImage("player3.png"),
-            loadImage("fly1.png"),
-            loadImage("fly2.png"),
-            loadImage("fly3.png"),
-            loadImage("grub1.png"),
-            loadImage("grub2.png"),
+            ImageManager.loadImage("player1.png"),
+            ImageManager.loadImage("player2.png"),
+            ImageManager.loadImage("player3.png"),
+            ImageManager.loadImage("fly1.png"),
+            ImageManager.loadImage("fly2.png"),
+            ImageManager.loadImage("fly3.png"),
+            ImageManager.loadImage("grub1.png"),
+            ImageManager.loadImage("grub2.png"),
         };
 
         images[1] = new Image[images[0].length];
@@ -185,11 +185,11 @@ public class TileMapManager {
         images[3] = new Image[images[0].length];
         for (int i=0; i<images[0].length; i++) {
             // right-facing images
-            images[1][i] = getMirrorImage(images[0][i]);
+/*             images[1][i] = getMirrorImage(images[0][i]);
             // left-facing "dead" images
             images[2][i] = getFlippedImage(images[0][i]);
             // right-facing "dead" images
-            images[3][i] = getFlippedImage(images[1][i]);
+            images[3][i] = getFlippedImage(images[1][i]); */
         }
 
         // create creature animations
@@ -197,24 +197,50 @@ public class TileMapManager {
         Animation[] flyAnim = new Animation[4];
         Animation[] grubAnim = new Animation[4];
         for (int i=0; i<4; i++) {
-            playerAnim[i] = createPlayerAnim(
+/*             playerAnim[i] = createPlayerAnim(
                 images[i][0], images[i][1], images[i][2]);
             flyAnim[i] = createFlyAnim(
                 images[i][3], images[i][4], images[i][5]);
             grubAnim[i] = createGrubAnim(
-                images[i][6], images[i][7]);
-        }
+                images[i][6], images[i][7]);*/
+        } 
 
         // create creature sprites
-        playerSprite = new Player(playerAnim[0], playerAnim[1],
-            playerAnim[2], playerAnim[3]);
-        flySprite = new Fly(flyAnim[0], flyAnim[1],
-            flyAnim[2], flyAnim[3]);
-        grubSprite = new Grub(grubAnim[0], grubAnim[1],
-            grubAnim[2], grubAnim[3]);
+        flySprite = new Sprite(flyAnim[0]);
+        grubSprite = new Sprite(grubAnim[0]);
 System.out.println("loadCreatureSprites successfully executed.");
 
     }
-*/
+
+
+/*     private Animation createGrubAnim(Image image, Image image2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createGrubAnim'");
+    }
+
+
+    private Animation createFlyAnim(Image image, Image image2, Image image3) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createFlyAnim'");
+    }
+
+
+    private Animation createPlayerAnim(Image image, Image image2, Image image3) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createPlayerAnim'");
+    }
+
+
+    private Image getFlippedImage(Image image) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getFlippedImage'");
+    }
+
+
+    private Image getMirrorImage(Image image) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMirrorImage'");
+    } */
+
 
 }
